fda40f02b592c89baa6c1314f067020a0487166a
Jack Speake <jackspeake@gmail.com>
2024-02-26 17:00:43 -0500

    prep work for virusi

--- Assets/Scripts/Viruses/PlayerHealthModule.cs
+++ Assets/Scripts/Viruses/PlayerHealthModule.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This simply controls the module that visualizes system health
 public class PlayerHealthModule : MonoBehaviour
 {
     // Start is called before the first frame update
--- Assets/Scripts/Viruses/VirusController.cs
+++ Assets/Scripts/Viruses/VirusController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class will control all references to system health and virus spawning
 public class VirusController : MonoBehaviour
 {
     // Start is called before the first frame update
