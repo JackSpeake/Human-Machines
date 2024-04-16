d2b9be49e67338d5ec80f6daa460bce04f992ee7
Jack Speake <35775453+JackSpeake@users.noreply.github.com>
2024-04-16 15:47:49 -0400

    handbook works

--- Assets/Scripts/Modules/HandbookModule.cs
+++ Assets/Scripts/Modules/HandbookModule.cs
@@ -11,22 +11,28 @@ public class HandbookModule : MonoBehaviour
     [TextArea]
     [SerializeField] private string overviewPage;
 
+    [TextArea]
+    [SerializeField] private string rulesPage;
+
     private GameManager gm;
 
     int stage = 1;
+    int page = 1;
 
-    [SerializeField] private TMPro.TMP_Text pageOne, pageTwo;
+    [SerializeField] private TMPro.TMP_Text pageOne, pageTwo, pageThree;
 
     // Start is called before the first frame update
     void Start()
     {
         pageOne.text = overviewPage;
+        pageThree.text = rulesPage;
         pageTwo.text = userPages[stage - 1];
         gm = GameManager.Instance;
         stage = 0;
 
         pageOne.enabled = true;
         pageTwo.enabled = false;
+        pageThree.enabled = false;
     }
 
     // Update is called once per frame
@@ -41,9 +47,33 @@ public class HandbookModule : MonoBehaviour
             
     }
 
-    public void SwapPage()
+    public void SwapPage(int change)
     {
-        pageOne.enabled = !pageOne.enabled;
-        pageTwo.enabled = !pageTwo.enabled;
+        page += change;
+
+        if (page <= 0)
+            page = 3;
+        else if (page > 3)
+            page = 1;
+
+        switch (page)
+        {
+            case 1:
+                pageOne.enabled = true;
+                pageTwo.enabled = false;
+                pageThree.enabled = false;
+                break;
+            case 2:
+                pageOne.enabled = false;
+                pageTwo.enabled = true;
+                pageThree.enabled = false;
+                break;
+            case 3:
+                pageOne.enabled = false;
+                pageTwo.enabled = false;
+                pageThree.enabled = true;
+                break;
+        }
+           
     }
 }
